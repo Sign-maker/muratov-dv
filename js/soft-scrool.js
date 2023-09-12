c9ad29f56d00cca22d129de839aaeb381eb37a07
Sign-maker <muratovdv76@gmail.com>
2023-09-13 02:05:00 +0300

    145

---
+++ js/soft-scrool.js
@@ -0,0 +1,16 @@
+/*========================SoftScrool===================*/
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substring(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});

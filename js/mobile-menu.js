a1b4297e22f3c2ae95df62900dcbc406b936ace8
Sign-maker <muratovdv76@gmail.com>
2023-10-05 11:59:58 +0300

    12

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -7,14 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
     menuBtn.addEventListener("click", function () {
       menu.classList.toggle("active"); // Toggle menu visibility
       menuBtn.classList.toggle("active-icon"); // Toggle menu icon state
-      body.classList.toggle("lock"); // Toggle body scrolling
+      // body.classList.toggle("lock"); // Toggle body scrolling
     });
   }
   menu.querySelectorAll(".menu-link").forEach((link) => {
     link.addEventListener("click", function () {
       menu.classList.remove("active"); // Toggle menu visibility
       menuBtn.classList.remove("active-icon"); // Toggle menu icon state
-      body.classList.remove("lock"); // Toggle body scrolling
+      // body.classList.remove("lock"); // Toggle body scrolling
     });
   });
 });
